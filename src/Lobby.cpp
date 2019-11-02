3079b1d40ae01021101fcb050b3c65c10056637c
Howard <hyuan6@ucmereced.edu>
2019-11-01 18:09:56 -0700

    Fixed inaccurate cell num. made  first click safe

--- src/Lobby.cpp
+++ src/Lobby.cpp
@@ -29,10 +29,18 @@ minesweeper::json lobby::joinLobby(Player p){
 }
 
 void lobby::genMines(){
-    for(int i = 0 ; i < NUM_MINES ; i++){
+    // for(int i = 0 ; i < NUM_MINES ; i++){
+    //     int r = std::rand() % BOARD_N;
+    //     int c = std::rand() % BOARD_M;
+    //     mineLoc.push_back(std::make_tuple(r,c));
+    // }
+    while(mineLoc.size() < NUM_MINES){
         int r = std::rand() % BOARD_N;
         int c = std::rand() % BOARD_M;
-        mineLoc.push_back(std::make_tuple(r,c));
+        std::tuple<int, int> location = std::make_tuple(r,c);
+        if(std::find(mineLoc.begin(), mineLoc.end(), location) == mineLoc.end()){
+            mineLoc.push_back(std::make_tuple(r,c));
+        }
     }
 }
 
